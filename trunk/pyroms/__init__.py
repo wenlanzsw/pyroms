5d4fcc591b3a7c8f056b5191a606952b6bf0aaa0
hetland <hetland@9af08260-d128-0410-9285-6dff05a3c1a6>
2007-02-07 23:29:01 +0000

    modified write_roms_grid method to better handle masked arrays.  Masked points are now interpolated to (an average of the) nearest points.

--- trunk/pyroms/__init__.py
+++ trunk/pyroms/__init__.py
@@ -1,4 +1,4 @@
-# ROMS class
+# PYROMS class
 # (c) Rob Hetland, 2005
 ''' 
 PYROMS
@@ -10,7 +10,7 @@ from roms_tools import zatr, zatw, scoordr, scoordw, isoslice, shrink, gc_dist
 from velocity import velocity
 from polygeom import Polygeom
 from polyclick import PolyClick
-from grid import gridgen, Grid
+from grid import Grid, gridgen, nc_grid
 
 __authors__ = ['Robert Hetland <hetland@tamu.edu>']
-__version__ = '0.4.0'
+__version__ = '0.4.5'
