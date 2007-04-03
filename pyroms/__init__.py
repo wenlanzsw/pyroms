0765eaebcc2f6ebb77e890e076e881e6bb5ca385
hetland <hetland@9af08260-d128-0410-9285-6dff05a3c1a6>
2007-04-03 16:27:24 +0000

    bug fix for importing new functions in roms_tools (and deleted Iso package)

--- pyroms/__init__.py
+++ pyroms/__init__.py
@@ -6,14 +6,14 @@ PYROMS
 
 from Dataset import Dataset
 from roms_time import roms_time
-from roms_tools import zatr, zatw, scoordr, scoordw, isoslice, shrink, gc_dist, rot2d
+from roms_tools import zatr, zatw, scoordr, scoordw, isoslice, shrink, gc_dist, \
+                       rot2d, zslice, iso_integrate, surface
 from velocity import velocity
 from polygeom import Polygeom
 from polyclick import PolyClick
 from grid import Grid, gridgen, nc_grid
 from ocean import rho_stp, o2_sat
 from depths import Depths, nc_depths
-from iso import Iso
 
 __authors__ = ['Robert Hetland <hetland@tamu.edu>']
 __version__ = '0.4.5'
