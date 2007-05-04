776445c3244a561abee96f3498722172696968b4
hetland <hetland@9af08260-d128-0410-9285-6dff05a3c1a6>
2007-05-04 18:14:11 +0000

    changes to velocity

--- pyroms/__init__.py
+++ pyroms/__init__.py
@@ -17,7 +17,7 @@ from ocean_time import ocean_time
 from velocity import velocity
 
 from roms_tools import zatr, zatw, scoordr, scoordw, isoslice, shrink, gc_dist, \
-                       rot2d, zslice, iso_integrate, surface
+                       rot2d, zslice, iso_integrate, surface, N2
 
 from ocean import rho_stp, o2_sat
 from polygeom import Polygeom
