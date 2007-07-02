7fed561327efb28546c695589f85ab0a6450cd7d
hetland <hetland@9af08260-d128-0410-9285-6dff05a3c1a6>
2007-07-02 17:33:48 +0000

    Added nc_ utilities, such as nc_pstrain to calculate principal strain.

--- pyroms/__init__.py
+++ pyroms/__init__.py
@@ -18,7 +18,7 @@ from velocity import velocity
 
 from roms_tools import zatr, zatw, scoordr, scoordw, isoslice, shrink, gc_dist, \
                        rot2d, zslice, iso_integrate, surface, N2, nc_N2, \
-                       nc_gls_dissipation
+                       nc_gls_dissipation, nc_curl, nc_div, nc_pstrain
 
 from ocean import rho_stp, o2_sat
 from polygeom import Polygeom
